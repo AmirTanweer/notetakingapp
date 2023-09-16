7de0e0ca567e101d3575271c3c3631a2bb4dc977
AmirTanweer <amirtanweer0@gmail.com>
2023-09-16 12:38:55 +0530

    created updatenote endpoint

--- routes/notes.js
+++ routes/notes.js
@@ -60,6 +60,48 @@ router.post('/addnote',fetchuser,[
     } 
     
 })
+//ROUTE 3: Updating an Existing Note using: PUT "/api/detail/updatenote/:id" Login required
+router.put('/updatenote/:id',fetchuser, async(req,res)=>{
+  
+
+    try{
+
+        const {title,description,tag}=req.body;
+        
+        //Create a newNote object
+        const newNote={};
+    if(title){
+        newNote.title=title
+    }
+    if(description){
+        newNote.description=description
+    }
+    if(tag){
+        newNote.tag=tag
+    }
+
+    //Find the note to be updated and update it
+    let Note= await notes.findById(req.params.id)
+    if(!Note){
+        return res.status(404).send("Not Found"); 
+    }
+    if(Note.user.toString()!=req.user.id)
+    {
+        return res.status(401).send("Not Allowed");
+    }
+    
+    Note=await notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+    res.json(Note) 
+     
+}
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal Server Error")
+    } 
+    
+})
+
+
 
 
 
